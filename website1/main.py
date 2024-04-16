60f67d38d9af04e2dad9ab71b046ee882752f28d
akhadse <akhadse@go.olemiss.edu>
2024-04-16 14:53:00 -0500

    Tried adding a loading page till video is ready but was not successful

--- website1/main.py
+++ website1/main.py
@@ -8,38 +8,50 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    if request.method == 'POST':
+    if request.method == 'POST':     
 
         form_data = request.form.to_dict()
 
-        form_data["choose_parameter_to_vary"] = str(form_data["choose_parameter_to_vary"])
-        form_data["parameter_start_value"] = float(form_data["parameter_start_value"])
-        form_data["parameter_end_value"] = float(form_data["parameter_end_value"])
-        form_data["parameter_steps"] = float(form_data["parameter_steps"])
-        form_data["l"] = int(form_data["l"])
-        form_data["m"] = int(form_data["m"])
+        choose_parameter_to_vary = str(form_data["choose_parameter_to_vary"])
+        parameter_start_value = float(form_data["parameter_start_value"])
+        parameter_end_value = float(form_data["parameter_end_value"])
+        parameter_steps = int(form_data["parameter_steps"])
+        l = int(form_data["l"])
+        m = int(form_data["m"])
 
-        form_data["q"] = float(form_data["q"])
-        form_data["chiAx"] = float(form_data["chiAx"])
-        form_data["chiAy"] = float(form_data["chiAy"])
-        form_data["chiAz"] = float(form_data["chiAz"])
-        form_data["chiBx"] = float(form_data["chiBx"])
-        form_data["chiBy"] = float(form_data["chiBy"])
-        form_data["chiBz"] = float(form_data["chiBz"])
+        q = float(form_data["q"])
+        chiAx = float(form_data["chiAx"])
+        chiAy = float(form_data["chiAy"])
+        chiAz = float(form_data["chiAz"])
+        chiBx = float(form_data["chiBx"])
+        chiBy = float(form_data["chiBy"])
+        chiBz = float(form_data["chiBz"])
 
-        form_data["f_low"] = float(form_data["f_low"])
-        form_data["delta_t"] = float(form_data["delta_t"])
-        form_data["sur_name"] = str(form_data["sur_name"])
+        f_low = float(form_data["f_low"])
+        delta_t = float(form_data["delta_t"])
+        sur_name = str(form_data["sur_name"])
 
-        form_data["video_width"] = float(form_data["video_width"])
-        form_data["video_height"] = float(form_data["video_height"])
-        form_data["video_fps"] = float(form_data["video_fps"])
-        form_data["video_name"] = str(form_data["video_name"])
+        video_width = int(form_data["video_width"])
+        video_height = int(form_data["video_height"])
+        video_fps = float(form_data["video_fps"])
+        video_name = str(form_data["video_name"])
 
-        form_data["figure_width"] = float(form_data["figure_width"])
-        form_data["figure_height"] = float(form_data["figure_height"])
+        figure_width = float(form_data["figure_width"])
+        figure_height = float(form_data["figure_height"])
+
+
+        user_input = {"q":q, 
+              "chiAx":chiAx, "chiAy":chiAy, "chiAz":chiAz, 
+              "chiBx":chiBx, "chiBy":chiBy, "chiBz":chiBz, 
+              "choose_parameter_to_vary":choose_parameter_to_vary, 
+              "parameter_start_value":parameter_start_value, "parameter_end_value":parameter_end_value, 
+              "parameter_steps":parameter_steps, "l":l, "m":m}
+        surrogate_params = {"sur_name":sur_name, "f_low":f_low, "delta_t":delta_t }
+        video_params = {"video_width":video_width, "video_height":video_height, "video_fps":video_fps, "video_name":video_name }
+        figure_params = {"figure_width":figure_width, "figure_height":figure_height}
+        input_dict = {"user_input":user_input, "surrogate_params":surrogate_params, "video_params":video_params,"figure_params":figure_params}
         
-        output = generate_video_for_varying_param(form_data)
+        output = generate_video_for_varying_param(input_dict)
         return render_template('result.html', output=output)
 
 if __name__ == '__main__':
