b107f0e0b03c717596913108470c30a5bfdfe074
akhadse <akhadse@go.olemiss.edu>
2024-04-25 19:25:24 -0500

    now downloading video for strain works

--- website2/website/pages.py
+++ website2/website/pages.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .utils import generate_video_for_varying_param,generate_sur_h_lm_for_param, generate_strain_for_param
+from .utils import generate_video_for_h_lm_varying_param, generate_sur_h_lm_for_param, generate_strain_for_param,generate_video_for_strain_for_varying_param
 
 pages = Blueprint('pages', __name__)
 
@@ -29,7 +29,52 @@ def download_strain():
     if request.method == 'POST':
         print("=================")
         print("Started making video -----")
-        print("=================")  
+        print("=================")      
+        
+        form_data = request.form.to_dict()
+
+        choose_parameter_to_vary = str(form_data["choose_parameter_to_vary"])
+        parameter_start_value    = float(form_data["parameter_start_value"])
+        parameter_end_value      = float(form_data["parameter_end_value"])
+        parameter_steps          = int(form_data["parameter_steps"])
+        
+        inclination              = float(form_data["inclination"])
+        phi_ref                  = float(form_data["phi_ref"])
+
+        q     = float(form_data["q"])
+        chiAx = float(form_data["chiAx"])
+        chiAy = float(form_data["chiAy"])
+        chiAz = float(form_data["chiAz"])
+        chiBx = float(form_data["chiBx"])
+        chiBy = float(form_data["chiBy"])
+        chiBz = float(form_data["chiBz"])
+
+        f_low    = float(form_data["f_low"])
+        delta_t  = float(form_data["delta_t"])
+        sur_name = str(form_data["sur_name"])
+
+        video_width  = int(form_data["video_width"])
+        video_height = int(form_data["video_height"])
+        video_fps    = float(form_data["video_fps"])
+        video_name   = str(form_data["video_name"])
+
+        user_input = {"q":q, 
+              "chiAx":chiAx, "chiAy":chiAy, "chiAz":chiAz, 
+              "chiBx":chiBx, "chiBy":chiBy, "chiBz":chiBz, 
+              "choose_parameter_to_vary":choose_parameter_to_vary, 
+              "parameter_start_value":parameter_start_value, "parameter_end_value":parameter_end_value, 
+              "parameter_steps":parameter_steps, "inclination":inclination, "phi_ref":phi_ref}
+        surrogate_params = {"sur_name":sur_name, "f_low":f_low, "delta_t":delta_t }
+        video_params = {"video_width":video_width, "video_height":video_height, "video_fps":video_fps, "video_name":video_name }
+        figure_params = {}
+        input_dict = {"user_input":user_input, "surrogate_params":surrogate_params, "video_params":video_params,"figure_params":figure_params}
+        
+        output = generate_video_for_strain_for_varying_param(input_dict)
+        print("=================")
+        print("Done!!!")
+        print("=================")
+        return render_template('base.html')
+
     else:
         return render_template('download_strain.html')
     
@@ -81,7 +126,7 @@ def download_h_lm():
         figure_params = {}
         input_dict = {"user_input":user_input, "surrogate_params":surrogate_params, "video_params":video_params,"figure_params":figure_params}
         
-        output = generate_video_for_varying_param(input_dict)
+        output = generate_video_for_h_lm_varying_param(input_dict)
         print("=================")
         print("Done!!!")
         print("=================")
